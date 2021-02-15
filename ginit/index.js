92c2dc8f2562a0517ba8802a5b9db1c211e556f0
timeonator <timeonator@gmail.com>
2021-02-14 17:31:47 -0800

    added node-fetch and dotenv

--- ginit/index.js
+++ ginit/index.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const files=require('../lib/files');
 const github = require('../lib/github');
+const fetch = require('node-fetch');
+
+ const token = process.env.GIT_TOKEN
 
 clear();
 
@@ -11,14 +15,18 @@ console.log(
     figlet.textSync('Code Garden', { horizontalLayout: 'full' })
   )
 );
-
+console.log(process.env.GIT_TOKEN);
 const run = async () => {
-    let token = github.getStoredGithubToken();
-    if(!token) {
-      token = await github.getPersonalAccesToken();
-    }
-    console.log(token);
-  };
+    const response = await fetch(
+        "https://api.github.com/repos/octokit/core.js/releases/latest", 
+        {
+          headers: {
+            authorization: `token ${process.env.GIT_TOKEN}`
+          }
+        }
+      )
+      console.log(await response.json());
+}
 
   run();
 
